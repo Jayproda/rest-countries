d1cef201893786ef35609a7f6f17102268b9baae
jayproda <jessietjohns97@gmail.com>
2026-09-15 11:52:22 +0100

    refactor: vercel uses root deployment while pages uses repo name

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/rest-countries.github.io/",
+  base: process.env.VERCEL ? "/" : "/rest-countries.github.io/",
   plugins: [react(), tailwindcss()],
 });
